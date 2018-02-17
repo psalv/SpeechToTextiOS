4a4484c1ab7db2ef8f9c0835dc8c9172f4402581
psalv <psalvato@uwo.ca>
2018-02-17 18:05:26 -0500

    working speech to text

--- SpeechRecognition/ViewController.swift
+++ SpeechRecognition/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        self.recordAndRecognizeSpeech()
+    }
+    
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {
+            buffer, _ in self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                let bestString = result.bestTranscription.formattedString
+                self.detectedTextLabel.text = bestString
+                
+                var lastString: String = ""
+                for segment in result.bestTranscription.segments {
+                    let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
+                    lastString = bestString.substring(from: indexTo)
+                }
+                
+                print(lastString)
+                
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
